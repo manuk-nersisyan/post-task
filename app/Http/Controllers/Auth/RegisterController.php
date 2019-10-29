2e658fb9dde1e00df7528fff14e1a33689e10def
manuk <manuk.nersisyan.96@gmail.com>
2019-10-29 23:42:04 +0400

    add login/registration 2 hours

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,11 +49,14 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name' => ['required', 'string', 'min:3','max:255'],
+            'surname' => ['nullable','string', 'min:5', 'max:255'],
+            'gender' => ['required' ,'string','in:female,male'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
     }
+
 
     /**
      * Create a new user instance after a valid registration.
@@ -64,8 +67,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
             'email' => $data['email'],
+            'name' => $data['name'],
+            'surname' => $data['surname'],
+            'gender' => $data['gender'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
             $table->string('email')->unique();
+            $table->string('name');
+            $table->string('surname')->nullable();
+            $table->string('gender');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
